42f9d884e3d82aa9932b363756aa1b6cf7ded971
Daniel Gomez <dinl_@hotmail.com>
2020-02-03 21:13:22 -0500

    Se ajusta para que quitar el cron

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
-const cron = require('node-cron');
 const { exec } = require('child_process');
 const axios = require('axios')
 
-cron.schedule('* * * * *', () => {
-
-    exec('dht-sensor 18 DHT22', (err, stdout, stderr) => {
-        if (err) {
-            //some err occurred
-            console.error(err)
-        } else {
-            let temperature_value = stdout.match(/(?<=temperature:\s).\S+/);
-            let humidity_value = stdout.match(/(?<=temperature:\s).\S+/);
-            
-            console.log(`Temperature: ${temperature_value} , Humidity:${humidity_value}`);
-            
-            axios.post('https://radiant-bayou-94606.herokuapp.com/temperature/insert', {
-                temperature: temperature_value[0],
-                humidity: humidity_value[0]
-            })
-            .then((res) => {
-                console.log(`Temp: ${temperature_value[0]} Hum: ${humidity_value[0]} - ${res.statusCode}`)
-            })
-            .catch((error) => {
-                console.error(error)
-            })
-        }
-    });
+exec('dht-sensor 18 DHT22', (err, stdout, stderr) => {
+    if (err) {
+        //some err occurred
+        console.error(err)
+    } else {
+        let temperature_value = stdout.match(/(?<=temperature:\s).\S+/);
+        let humidity_value = stdout.match(/(?<=temperature:\s).\S+/);
+        
+        console.log(`Temperature: ${temperature_value} , Humidity:${humidity_value}`);
+        
+        axios.post('https://radiant-bayou-94606.herokuapp.com/temperature/insert', {
+            temperature: temperature_value[0],
+            humidity: humidity_value[0]
+        })
+        .then((res) => {
+            console.log(`Temp: ${temperature_value[0]} Hum: ${humidity_value[0]} - ${res.statusCode}`)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+    }
 });
